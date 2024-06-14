7f5781470026ded6c5179c9f0aa49e6074840a85
Tiaraarredondo <tarredondo@udesa.edu.ar>
2024-06-14 10:42:00 -0300

    index seccion2

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,7 @@
+
 //ACCESORIOS
 document.addEventListener('DOMContentLoaded', function () {
-    let accesoryIn = document.querySelector("#accesorios-container");
+    let accesoryIn = document.querySelector("#jewelery");
 
     fetch('https://fakestoreapi.com/products/category/jewelery')
         .then(function (res) {
@@ -38,7 +39,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 //MEN:
 document.addEventListener('DOMContentLoaded', function () {
-    let menIn = document.querySelector("#hombre-container");
+    let menIn = document.querySelector("#men");
 
     fetch("https://fakestoreapi.com/products/category/men's clothing")
         .then(function (res) {
@@ -49,16 +50,54 @@ document.addEventListener('DOMContentLoaded', function () {
 
             for (let i = 0; i < data.length && i < 5; i++) {
                 contenido += `
-                    <li>
+                    <div class="elemento-hijo">
                         <img src="${data[i].image}" alt="${data[i].title}" width="100" />
                         <p>${data[i].title}</p>
                         <p>Description: ${data[i].description}</p>
                         <p>$${data[i].price}</p>
                         <p>${data[i].category}</p>
                         <a href="./jewelery.html?id=${data[i].id}" class="ver-mas-boton">Ver más</a>
-                    </li>`;
+                    </div>`;
             }
             menIn.innerHTML = contenido;
+
+            let verButtons = document.querySelectorAll(".ver-mas-boton");
+            verMasButtons.forEach(button => {
+                button.addEventListener("click", function(event) {
+                    let url = this.getAttribute("href");
+                    window.location.href = url;
+                });
+            });
+
+        })
+        .catch(function (error) {
+            console.error('Error fetching products:', error);
+        });
+});
+
+/*MUJER*/
+document.addEventListener('DOMContentLoaded', function () {
+    let womenIn = document.querySelector("#women");
+
+    fetch("https://fakestoreapi.com/products/category/women's clothing")
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (data) {
+            let contenido = "";
+
+            for (let i = 0; i < data.length && i < 5; i++) {
+                contenido += `
+                    <div class="elemento-hijo">
+                        <img src="${data[i].image}" alt="${data[i].title}" width="100" />
+                        <p>${data[i].title}</p>
+                        <p>Description: ${data[i].description}</p>
+                        <p>$${data[i].price}</p>
+                        <p>${data[i].category}</p>
+                        <a href="./jewelery.html?id=${data[i].id}" class="ver-mas-boton">Ver más</a>
+                    </div>`;
+            }
+            womenIn.innerHTML = contenido;
 
             let verMasButtons = document.querySelectorAll(".ver-mas-boton");
             verMasButtons.forEach(button => {
