2b0a2e21260948619db9f13d8bb218e6e207e940
sofiagarziaa <sgarzia@udesa.edu.ar>
2024-06-21 14:58:41 -0300

    carrito

--- js/producto.js
+++ js/producto.js
@@ -1,47 +1,47 @@
 console.log("producto.js");
 
-document.addEventListener("DOMContentLoaded", function() {
-    let qs = location.search;
-    console.log("query string", qs);
-    let qsto = new URLSearchParams(qs);
-    const productId = qsto.get('id');
 
-    let apiUrl = `https://fakestoreapi.com/products/${productId}`;
-    if (productId) {
-        fetch(apiUrl)
-            .then(response => response.json())
-            .then(data => {
-                let title = document.querySelector(".product-title");
-                let image = document.querySelector(".product-image");
-                let categoryLink = document.querySelector('.category-link');
-                let status = document.querySelector(".product-category");
-                let especie = document.querySelector(".product-price");
+let qs = location.search;
+console.log("query string", qs);
+let qsto = new URLSearchParams(qs);
+const productId = qsto.get('id');
 
-                title.innerText = data.title;
-                categoryLink.href = `./category.html?category=${data.category}`;
-                image.src = data.image;
-                status.innerText = `Categoría: ${data.category}`;
-                especie.innerText = `Precio: $${data.price}`;
+let apiUrl = `https://fakestoreapi.com/products/${productId}`;
+if (productId) {
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            let title = document.querySelector(".product-title");
+            let image = document.querySelector(".product-image");
+            let categoryLink = document.querySelector('.category-link');
+            let status = document.querySelector(".product-category");
+            let especie = document.querySelector(".product-price");
 
-                let buyBtn = document.querySelector(".buy-btn.add-to-cart");
-                buyBtn.addEventListener("click", function(e) {
-                    e.preventDefault();
-                    agregarAlCarrito(data);
-                });
-            })
-            .catch(error => console.error(error));
-});
+            title.innerText = data.title;
+            categoryLink.href = `./category.html?category=${data.category}`;
+            image.src = data.image;
+            status.innerText = `Categoría: ${data.category}`;
+            especie.innerText = `Precio: $${data.price}`;
 
-function agregarAlCarrito(producto) {
-    let productosEnCarrito = localStorage.getItem("carItem");
-    let carrito = [];
-
-    if (productosEnCarrito !== null) {
-        carrito = JSON.parse(productosEnCarrito);
-    }
-
-    carrito.push(producto.id);
-    localStorage.setItem("carItem", JSON.stringify(carrito));
-    alert(`Producto "${producto.title}" añadido al carrito`);
-    console.log("Carrito actualizado:", carrito);
+            let buyBtn = document.querySelector(".buy-btn.add-to-cart");
+            buyBtn.addEventListener("click", function (e) {
+                e.preventDefault();
+                agregarAlCarrito(data);
+            });
+        })
+        .catch(error => console.error(error));
 }
+
+    function agregarAlCarrito(producto) {
+        let productosEnCarrito = localStorage.getItem("carItem");
+        let carrito = [];
+
+        if (productosEnCarrito !== null) {
+            carrito = JSON.parse(productosEnCarrito);
+        }
+
+        carrito.push(producto.id);
+        localStorage.setItem("carItem", JSON.stringify(carrito));
+        alert(`Producto "${producto.title}" añadido al carrito`);
+        console.log("Carrito actualizado:", carrito);
+    }
