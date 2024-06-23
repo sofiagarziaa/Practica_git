2bef5384fee3846ae528b1fcead7b31a6b50e940
Tiaraarredondo <tarredondo@udesa.edu.ar>
2024-06-22 21:07:41 -0300

    boton de categoria

--- js/producto.js
+++ js/producto.js
@@ -16,34 +16,45 @@ fetch(url)
     .then(function (data) {
         console.log(data);
         let title = document.querySelector("h2");
-        let image = document.querySelector(".image");
         let desc = document.querySelector(".desc");
         let price = document.querySelector(".price");
+        let image = document.querySelector(".image");
+
+        let categoryLink = document.querySelector("h3");
+
         title.innerText = data.title;
+        desc.innerText = data.description;
+        price.innerText = `$${data.price}`;
         image.src = data.image;
-        desc.innerText += data.description;
-        price.innerText += `$${data.price}`;
 
-    })
-    .catch(function (error) {
-        console.error(error);
-    })
+        let category = data.category;
+        categoryLink.innerHTML = `<a href="./category.html?category=${category}">${category}</a>`;
+        categoryLink.querySelector("a").addEventListener("click", function (e) {
+            e.preventDefault();
+            let linkURL = `./category.html?category=${category}`;
+            location.href = linkURL;
+        })
+        })
+        .catch(function (error) {
+                console.error(error);
+        });
+    
 
-let carrito = [];
-let recuperoStorage = localStorage.getItem("cartItems")
+        let carrito = [];
+        let recuperoStorage = localStorage.getItem("cartItems")
 
-if(recuperoStorage != null) {
-    carrito = JSON.parse(recuperoStorage)
-}
+        if (recuperoStorage != null) {
+            carrito = JSON.parse(recuperoStorage)
+        }
 
-let select = document.querySelector(".select")
-select.addEventListener("click", function(e){
-    e.preventDefault();
-    carrito.push(id)
-    let carStr = JSON.stringify(carrito)
-    localStorage.setItem("cartItems", carStr)
+        let select = document.querySelector(".select")
+        select.addEventListener("click", function (e) {
+            e.preventDefault();
+            carrito.push(id)
+            let carStr = JSON.stringify(carrito)
+            localStorage.setItem("cartItems", carStr)
 
-    console.log("carrito", carrito)
-    console.log("localStore: ", localStorage)
-})
+            console.log("carrito", carrito)
+            console.log("localStore: ", localStorage)
+        })
 
